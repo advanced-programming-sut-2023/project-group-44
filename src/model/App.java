f8deb68f81f9ce127c1e34936ecb22e820e58cb0
Ali Mohammadzade Shabestari <119702236+Almolia@users.noreply.github.com>
2023-04-17 23:18:05 +0430

    Update App.java

--- src/model/App.java
+++ src/model/App.java
@@ -6,4 +6,17 @@ public class App {
     private static User currentUser;
     private static ArrayList<User> users = new ArrayList<>();
     private static ArrayList<Requests> tradeList = new ArrayList<>();
+    
+    public static ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public static User getUserByUsername (String username){
+        for (User user : App.getUsers()){
+            if (user.getUsername().equals(username)){
+                return user;
+            }
+        }
+        return null;
+    }
 }
