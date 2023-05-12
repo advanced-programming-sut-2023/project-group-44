ae48e5727f4baff12b918fb57067ba70d0d3497d
nimah <nimahonarmand8383@gmail.com>
2023-05-12 07:29:09 -0700

    Things has changed

--- src/src/main/java/controller/MarketMenuController.java
+++ src/src/main/java/controller/MarketMenuController.java
@@ -9,24 +9,31 @@ public class MarketMenuController {
     public static String showPriceList(){
         String result="List of items:\n" +
                 "item's name           number of sales     buy price   sell price" +
-                "1) wood:                     10               3           2\n" +
-                "2) iron:                     10               8           6\n" +
-                "3) stone:                    10               5           3\n" +
-                "4) flour:                    20               5           4\n" +
-                "5) metal armour:             5                2           1\n" +
-                "6) sword:                    5                3           2\n" +
-                "7) mace:                     5                3           2\n" +
-                "8) bow:                      5                4           2\n" +
-                "9) spear:                    5                4           2\n" +
-                "10) melting pot:             4                2           1\n" +
-                "11) horse:                   3                2           1\n" +
-                "12) apple:                   50               5           3\n" +
-                "13) cheese:                  30               5           4\n" +
-                "14) leather vest:            10               2           1\n" +
-                "15) hop:                     25               5           4\n" +
-                "16) beer:                    15               5           4\n" +
-                "17) bread:                   15               5           4\n" +
-                "18) meat:                    7                2           1\n";
+                " ********************** resources ************************\n"+
+                " wood:                     10               3           2\n" +
+                " iron:                     10               8           6\n" +
+                " stone:                    10               5           3\n" +
+                " pitch:                    10               6           4\n" +
+                " ************************ foods **************************\n"+
+                " hop:                      25               5           4\n" +
+                " wheat:                    25               5           4\n" +
+                " flour:                    20               5           4\n" +
+                " apple:                    50               5           3\n" +
+                " meat:                     7                2           1\n" +
+                " cheese:                   30               5           4\n" +
+                " bread:                    15               5           4\n" +
+                " beer:                     15               5           4\n" +
+                " *********************** weapons *************************\n"+
+                " metal armour:             5                2           1\n" +
+                " sword:                    5                3           2\n" +
+                " mace:                     5                3           2\n" +
+                " bow:                      5                4           2\n" +
+                " spear:                    5                4           2\n" +
+                " melting pot:              4                2           1\n" +
+                " leather vest:             10               2           1\n" +
+                " ********************** animals *************************\n"+
+                " cow:                      3                2           1\n" +
+                " horse:                    3                2           1\n" ;
         return result;
     }
 
--- src/src/main/java/model/Things.java
+++ src/src/main/java/model/Things.java
@@ -1,32 +1,34 @@
 package model;
 
+import model.Buildings.Building;
+
 public enum Things {
 
     // Resources
-    WOOD("wood", "resource", 1000,10, 3, 2),
-    STONE("stone", "resource",500,10, 5, 3 ),
-    IRON("iron", "resource",250,10, 8, 6),
-    PITCH("pitch", "resource",750,10, 6, 4),
+    WOOD("wood", "resource","treasury", 1000,10, 3, 2),
+    STONE("stone", "resource","treasury",500,10, 5, 3 ),
+    IRON("iron", "resource","treasury",250,10, 8, 6),
+    PITCH("pitch", "resource","treasury",750,10, 6, 4),
     // Foods
-    WHEAT("wheat","raw", 1000,25, 5, 4),
-    HOP("hop","raw",1000,25,5,4),
-    FLOUR("flour", "raw",1000,20, 5, 4),
-    MEAT("meat", "food",50,7, 2, 1),
-    CHEESE("cheese",  "food",100,30, 5, 4),
-    APPLE("apple",  "food",100,50, 5, 3),
-    BREAD("bread",  "food",50,15, 5, 4),
-    BEER("beer",  "food",25,15, 5, 4),
+    WHEAT("wheat","food","food stockpile", 1000,25, 5, 4),
+    HOP("hop","food","food stockpile",1000,25,5,4),
+    FLOUR("flour", "food","food stockpile",1000,20, 5, 4),
+    MEAT("meat", "food","food stockpile",50,7, 2, 1),
+    CHEESE("cheese",  "food","food stockpile",100,30, 5, 4),
+    APPLE("apple",  "food","food stockpile",100,50, 5, 3),
+    BREAD("bread",  "food","food stockpile",50,15, 5, 4),
+    BEER("beer",  "food","food stockpile",25,15, 5, 4),
     // Herd
-    HORSE("horse", "herd", 10,3, 2, 1),
-    COW("cow", "herd", 3,3, 2, 1),
+    HORSE("horse", "herd","stable", 10,3, 2, 1),
+    COW("cow", "herd","stable", 3,3, 2, 1),
     // Weapons
-    BOW("bow", "weapon", 5,5, 4, 2),
-    SPEAR("spear",  "weapon", 5,5, 4, 2),
-    MACE("mace",  "weapon", 5,5, 3, 2),
-    SWORD("swords",  "weapon", 5,5, 3, 2),
-    LEATHER_ARMOR("Leather armor",  "weapon", 5,10, 2, 1),
-    METAL_ARMOR("metal armor",  "weapon", 5,5, 2, 1),
-    MELTING_POT("melting pot","weapon",5,4,2,1);
+    BOW("bow", "weapon","armory", 5,5, 4, 2),
+    SPEAR("spear",  "weapon","armory", 5,5, 4, 2),
+    MACE("mace",  "weapon","armory", 5,5, 3, 2),
+    SWORD("swords",  "weapon","armory", 5,5, 3, 2),
+    LEATHER_ARMOR("Leather armor",  "weapon","armory", 5,10, 2, 1),
+    METAL_ARMOR("metal armor",  "weapon","armory", 5,5, 2, 1),
+    MELTING_POT("melting pot","weapon","armory",5,4,2,1);
     private final String name;
     private final String type;
     private int count;
@@ -34,7 +36,7 @@ public enum Things {
     private final double purchasePrice;
     private final double sellPrice;
 
-    Things(String name, String type, int count, int numberOfSalesPerBuyOrSell, double purchasePrice, double sellPrice) {
+    Things(String name, String type, String storageName, int count, int numberOfSalesPerBuyOrSell, double purchasePrice, double sellPrice) {
         this.name = name;
         this.type = type;
         this.count = count;
