96a71ba48ca6ce057e41a195b65b88fd445a8675
AmirhoseinM82 <mirdariyanamirhosein@gmail.com>
2023-07-05 21:28:28 +0330

    change avatar

--- src/src/main/java/view/ProfileMenu/ChangeAvatar/ChangeAvatarMenu.java
+++ src/src/main/java/view/ProfileMenu/ChangeAvatar/ChangeAvatarMenu.java
@@ -1,14 +1,19 @@
 package view.ProfileMenu.ChangeAvatar;
 
+import controller.ProfileMenuController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import model.App;
 import view.Main;
 import view.ProfileMenu.ProfileMenu;
 
+import java.io.File;
 import java.net.URL;
 
 public class ChangeAvatarMenu extends Application {
@@ -26,6 +31,27 @@ public class ChangeAvatarMenu extends Application {
                 BackgroundSize.DEFAULT);
 
         Background bGround = new Background(bImg);
+
+        Button button = new Button("Select another avatar");
+        button.setOnAction(value ->  {
+            try {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Search Image");
+                fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Search image", "*.png", "*.jpg"));
+                File selectedFile = fileChooser.showOpenDialog(stage);
+
+                if (selectedFile != null) {
+                    Image image=new Image(getClass().getResource(selectedFile.toString()).toString());
+                    ProfileMenuController.addImageToProfile(image);
+                }
+            } catch (Exception e) {
+                System.out.println("You must handle the error: " + e.getMessage());
+            }
+        });
+
+
+        borderPane.setLeft(button);
+
         borderPane.setBackground(bGround);
         Scene scene=new Scene(borderPane);
         stage.setScene(scene);
